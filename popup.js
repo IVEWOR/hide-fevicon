50a0a6dd0f9e763fd4f390dee0bf1f873808cb36
IVEWOR <howdy@deepslog.com>
2024-06-22 20:51:05 +0530

    learning

---
+++ popup.js
@@ -0,0 +1,14 @@
+
+// // let fevicon = document.querySelector("link[rel~='icon']");
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let hideIcons = document.querySelector("#hideIcons");
+//     // onClick's logic below:
+//     hideIcons.addEventListener("click", function () {
+//         console.log("yeahhh1!!!")
+//     });
+
+//     console.log("dsdsdsd")
+// });
+
